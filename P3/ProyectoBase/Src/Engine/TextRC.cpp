a798da1b632b0cd78e56494c6ecc3fc527ad2337
onaranjoUCM <onaranjo@ucm.es>
2020-03-10 10:11:58 +0100

    freetype removed from project

--- P3/ProyectoBase/Src/Engine/TextRC.cpp
+++ P3/ProyectoBase/Src/Engine/TextRC.cpp
@@ -3,16 +3,7 @@
 #include <iostream>
 
 TextRC::TextRC() {
-  int error = FT_Init_FreeType(&library);
-  if (error) {
-    std::cout << "An error occurred during library initialization";
-  }
-
-  error = FT_New_Face(library, "../../assets/fonts/arial.ttf", 0, &face);
-  if (error == FT_Err_Unknown_File_Format) {
-    std::cout << "The font file could be opened and read, but it appears that its font format is unsupported";
-  }
-  else if (error) {
-    std::cout << "The font file could not be opened or read, or it is broken";
-  }
+  
 }
+
+TextRC::~TextRC() {}
--- P3/ProyectoBase/Src/Engine/TextRC.h
+++ P3/ProyectoBase/Src/Engine/TextRC.h
@@ -1,13 +1,8 @@
 #pragma once
 
-#include <ft2build.h>
-#include FT_FREETYPE_H
-
 class TextRC
 {
 private:
-  FT_Library library;
-  FT_Face face;
 
 public:
 
